2983bc6f009917c0292f6a5ecc38c434264201ad
Nicole Candiotti <nccandiotti@gmail.com>
2022-05-09 12:56:27 -0400

    added react example

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Hello world" />
+    </div>
+  )
+}
+
+export default App
